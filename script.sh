16619f93795e1301e836c910bc81c2d0934fc4ae
Jordan Webb <jordan@getseam.com>
2021-06-03 22:15:47 -0500

    shfmt

--- script.sh
+++ script.sh
@@ -52,15 +52,15 @@ fi
 
 echo '::group::🔎 Running pyright with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
-"$(npm bin)/pyright" "${PYRIGHT_ARGS[@]}" ${INPUT_PYRIGHT_FLAGS:-} \
-  | python3 "${BASE_PATH}/pyright_to_rdjson.py" \
-  | reviewdog -f=rdjson \
-      -name="${INPUT_TOOL_NAME}" \
-      -reporter="${INPUT_REPORTER:-github-pr-review}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+"$(npm bin)/pyright" "${PYRIGHT_ARGS[@]}" ${INPUT_PYRIGHT_FLAGS:-} |
+  python3 "${BASE_PATH}/pyright_to_rdjson.py" |
+  reviewdog -f=rdjson \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER:-github-pr-review}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS}
 
 reviewdog_rc=$?
 echo '::endgroup::'
