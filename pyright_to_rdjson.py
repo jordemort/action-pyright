ac63ed5a19e3bb378d952fb5d4d71d28744ca792
Jordan Webb <jordan@getseam.com>
2021-06-03 23:46:39 -0500

    Fix severity typo (#8)
    * Fix severity typo
    
    * Remove extra brackets

--- pyright_to_rdjson.py
+++ pyright_to_rdjson.py
@@ -20,7 +20,7 @@ def pyright_to_rdjson(jsonin: TextIO):
         rdjson["diagnostics"].append(
             {
                 "message": f"{d['message']} ({d['rule']})",
-                "severity": d["serverity"].upper(),
+                "severity": d["severity"].upper(),
                 "location": {
                     "path": d["file"],
                     "range": {
